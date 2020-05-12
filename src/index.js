321026dfa3674b3c52e24a313bbdcc418a933293
Ivan <fivanunam@gmail.com>
2020-05-12 11:13:07 -0500

    part 2 phaser game

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import 'phaser';
 import './assets/css/style.css';
+import sky from './assets/images/sky.png';
+import ground from './assets/images/platform.png';
+import start from './assets/images/star.png';
+import bomb from './assets/images/bomb.png';
+import dude from './assets/images/dude.png';
 
 var config = {
   type: Phaser.AUTO,
@@ -16,10 +21,20 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+  this.load.image('sky', sky);
+  this.load.image('ground', ground);
+  this.load.image('star', start);
+  this.load.image('bomb', bomb);
+  this.load.spritesheet('dude', 
+      dude,
+      { frameWidth: 32, frameHeight: 48 }
+  );
 }
 
 function create ()
 {
+  this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'star');
 }
 
 function update ()
