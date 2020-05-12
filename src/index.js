d1dbed529a500f4c8012e25f79505c66e64f2fa5
Ivan <fivanunam@gmail.com>
2020-05-12 15:18:08 -0500

    part 8 phaser game

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ let game = new Phaser.Game(config);
 let platforms;
 let player;
 let cursors;
+let stars;
 
 function preload ()
 {
@@ -39,6 +40,11 @@ function preload ()
       dude,
       { frameWidth: 32, frameHeight: 48 }
   );
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
 }
 
 function create ()
@@ -75,6 +81,17 @@ function create ()
   this.physics.add.collider(player, platforms);
 
   cursors = this.input.keyboard.createCursorKeys();
+
+  stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+  });
+  stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update ()
@@ -101,6 +118,6 @@ function update ()
 
   if (cursors.up.isDown && player.body.touching.down)
   {
-      player.setVelocityY(-330);
+      player.setVelocityY(-800);
   }
 }
