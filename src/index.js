6dbe09d50b230cc557b3518df4e372faa0abcf18
Ivan <fivanunam@gmail.com>
2020-05-12 18:56:55 -0500

    initial template

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import 'phaser';
 import './assets/css/style.css';
+import logoImg from "./assets/images/logo.png";
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("logo", logoImg);
+}
+
+function create() {
+  const logo = this.add.image(400, 150, "logo");
+
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1
+  });
+}
