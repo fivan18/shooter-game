dc1a689ec02c5ef68cba6b06b02b6402683ee6e8
Ivan <fivanunam@gmail.com>
2020-05-11 21:33:03 -0500

    part 1 phaser game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import { test } from './scripts/moduleTest'
+import 'phaser';
 
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
+  }
+};
 
-test();
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
