c302af22ea2402836fa6b886f8d17b0ec0913126
Ivan <fivanunam@gmail.com>
2020-05-12 18:18:46 -0500

    basic template config for phaser

--- src/scripts/moduleTest.js
+++
@@ -1,9 +0,0 @@
-function test() {
-  document.querySelector('.container').innerHTML = 'Hello world';
-}
-
-function sum(a, b) {
-  return a + b;
-}
-
-export { test, sum };
--- src/scripts/moduleTest.test.js
+++
@@ -1,5 +0,0 @@
-import { sum } from './moduleTest'
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
