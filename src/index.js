322e0be43cbaae332d2f7842a59a2cc4b0d5cec5
Ivan <fivanunam@gmail.com>
2020-05-12 19:42:31 -0500

    Update prject and create Scenes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
 import 'phaser';
 import './assets/css/style.css';
-import logoImg from "./assets/images/logo.png";
+import config from './scripts/config/config';
+import GameScene from './scripts/scenes/gameScene';
+import BootScene from './scripts/scenes/bootScene';
+import PreloaderScene from './scripts/scenes/preloaderScene';
+import TitleScene from './scripts/scenes/titleScene';
+import OptionsScene from './scripts/scenes/optionsScene';
+import CreditsScene from './scripts/scenes/creditsScene';
 
-const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
   }
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", logoImg);
 }
+ 
+window.game = new Game();
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
