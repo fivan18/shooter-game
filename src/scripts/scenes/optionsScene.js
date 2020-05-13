a5b79a71c35b301b1da524fbfe50aaa142ef040d
Ivan <fivanunam@gmail.com>
2020-05-13 14:27:19 -0500

    refactor options Scene for button menu

--- src/scripts/scenes/optionsScene.js
+++ src/scripts/scenes/optionsScene.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import Button from '../objects/button';
  
 export default class OptionsScene extends Phaser.Scene {
   constructor () {
@@ -50,13 +51,7 @@ export default class OptionsScene extends Phaser.Scene {
     }.bind(this));
 
     // return to title scene
-    this.menuButton = this.add.sprite(400, 500, 'blueButton1').setInteractive();
-    this.menuText = this.add.text(0, 0, 'Menu', { fontSize: '32px', fill: '#fff' });
-    Phaser.Display.Align.In.Center(this.menuText, this.menuButton);
-    
-    this.menuButton.on('pointerdown', function (pointer) {
-      this.scene.start('Title');
-    }.bind(this));
+    this.menuButton = new Button(this, 400, 500, 'blueButton1', 'blueButton2', 'Menu', 'Title');
 
     // update audio
     this.updateAudio();
