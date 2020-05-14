c55314f60c7c4a99c04153c15369cdbaa0c30940
Ivan <fivanunam@gmail.com>
2020-05-14 12:28:17 -0500

    add laser for player

--- src/scripts/scenes/gameScene.js
+++ src/scripts/scenes/gameScene.js
@@ -174,6 +174,14 @@ export default class GameScene extends Phaser.Scene {
       this.player.moveRight();
     }
 
+    if (this.keySpace.isDown) {
+      this.player.setData("isShooting", true);
+    }
+    else {
+      this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+      this.player.setData("isShooting", false);
+    }
+
     for (let i = 0; i < this.enemies.getChildren().length; i++) {
       let enemy = this.enemies.getChildren()[i];
 
