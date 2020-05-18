eb25cbd787f8782211001c3b0a33d9f1cf9bc8a0
Ivan <fivanunam@gmail.com>
2020-05-18 16:00:07 -0500

    fix linter

--- src/scripts/scenes/authenticationScene.js
+++ src/scripts/scenes/authenticationScene.js
@@ -1,6 +1,5 @@
 /* global Phaser */
-/* eslint import/no-unresolved: [2, { ignore: ['^phaser$'] }] */
-/* eslint import/no-unresolved: [2, { ignore: ['^phaser3-rex-plugins\/plugins\/inputtext$'] }] */
+/* eslint import/no-unresolved: [2, { ignore: ['^(phaser3-rex-plugins\/plugins\/inputtext$ | ^phaser$)'] }] */
 
 import 'phaser';
 import InputText from 'phaser3-rex-plugins/plugins/inputtext'; // eslint-disable-line no-unused-vars
